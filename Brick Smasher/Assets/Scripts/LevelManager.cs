06300aa067f3d3d44d3f7028e3a02322d6b4fda6
Szymon Duchniewicz <willmiszgaming@gmail.com>
2017-03-23 20:27:55 +0100

    Brick Smasher v0.01

---
+++ Brick Smasher/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadLevel(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+	
+	// Update is called once per frame
+	public void Quit()
+    {
+        Application.Quit();
+    }
+}
