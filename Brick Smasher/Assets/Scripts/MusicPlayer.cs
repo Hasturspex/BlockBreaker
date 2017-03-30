f14f0497e061ff9cf9051830df928c53ce57359b
Szymon Duchniewicz <willmiszgaming@gmail.com>
2017-03-30 20:24:17 +0200

    Brick Smasher v0.02

---
+++ Brick Smasher/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+	// Use this for initialization
+
+        void Awake ()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+}
